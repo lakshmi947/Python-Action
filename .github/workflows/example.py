07286a92c7aeb0c05cf186bf429621307d06b1c7
lakshmi947 <54805596+lakshmi947@users.noreply.github.com>
2023-09-12 22:51:34 +0530

    added token example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,4 +1,9 @@
+import os
 def main():
   print("Hello Python Github Actions!")
+  token = os.environ.get("PYTHON_SECRET_TOKEN")
+  if not token:
+    raise RuntimeError("PYTHON_SECRET_TOKEN is not set!")
+  print("Token is set")
 if __name__ == '__main__':
   main()
